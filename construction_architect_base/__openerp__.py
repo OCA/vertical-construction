d5986d640e9c51ffdf2dc4aeac04a58d7fbf090a
mathi123 <colpaert.mathias@gmail.com>
2015-04-25 15:42:14 +0200

    [ADD] Base architect module

---
+++ construction_architect_base/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OCA Construction: Architect Base",
+
+    'summary': """
+        Identify partners as architects.""",
+
+    'description': """
+        This module allows you to:
+            - Identify a partner as an architect.
+            - Filter architects from address book.
+    """,
+
+    'author': "Mathias Colpaert",
+
+    'category': "construction",
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': ['views.xml'],
+}
