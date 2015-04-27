b3637d7411e58c6c771226e4bdc14e50e643c604
mathi123 <colpaert.mathias@gmail.com>
2015-04-27 17:29:29 +0200

    [REM] Remove modules

--- construction_architect_project/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields
-
-class Project(models.Model):
-    _name = 'project.project'
-    _inherit = 'project.project'
-
-    architect = fields.Many2one('res.partner', domain="[('isArchitect', '=', '1')]")
--- construction_architect_sale/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields
-
-class Lead(models.Model):
-    _name = 'crm.lead'
-    _inherit = 'crm.lead'
-
-    architect = fields.Many2one('res.partner', domain="[('isArchitect', '=', '1')]")
