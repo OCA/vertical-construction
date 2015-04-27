904556d5cd87cb05b89dd23adac057cacb170324
mathi123 <colpaert.mathias@gmail.com>
2015-04-27 17:37:10 +0200

    [ADD] Architect in sale module

---
+++ construction_architect_sale/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OCA Construction: Architect Sale",
+
+    'summary': """
+        Indicate the architect of an opportunity.""",
+
+    'description': """
+        This module allows you to:
+            - Identify the architect of a lead/opportunity.
+            - Filter/group/search opportunities by architect.
+    """,
+
+    'author': "Mathias Colpaert",
+
+    'category': "construction",
+
+    'version': '0.1',
+
+    'depends': ['crm', 'construction_architect_base'],
+
+    'data': ['views.xml'],
+}
