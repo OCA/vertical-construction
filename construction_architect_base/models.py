1e2fcee80215945926e9e7070112c3ff5941379c
Mathias Colpaert <colpaert.mathias@gmail.com>
2015-04-28 20:17:06 +0200

    Revert "[ADD] base - architect module"

--- construction_architect_base/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields
-
-class Partner(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
-
-    isArchitect = fields.Boolean()
