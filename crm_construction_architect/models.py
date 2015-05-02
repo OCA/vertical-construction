a7cc766c96e2ba517a944ce8126d8f27a6be5e60
mathi123 <colpaert.mathias@gmail.com>
2015-05-02 09:14:03 +0200

    [FIX] PEP8

--- crm_construction_architect/models.py
+++ crm_construction_architect/models.py
@@ -7,4 +7,5 @@ class Lead(models.Model):
     _name = 'crm.lead'
     _inherit = 'crm.lead'
 
-    architect = fields.Many2one('res.partner', domain="[('isArchitect', '=', '1')]")
+    architect = fields.Many2one('res.partner',
+                                domain="[('isArchitect', '=', '1')]")
