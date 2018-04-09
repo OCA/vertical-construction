9d17bb32b7c82fd70f886ceadd72305a132742f9
b5baxter <robert@vrec.ca>
2018-04-08 21:14:28 -0700

    [MIG] base_construction_architect: Migration to 10.0

--- base_construction_architect/__manifest__.py
+++ base_construction_architect/__manifest__.py
@@ -27,8 +27,8 @@
     """,
     'author': "Mathias Colpaert, Odoo Community Association (OCA)",
     'category': "construction",
-    'version': '8.0.0.1.0',
+    'version': '10.0.0.1.0',
     'depends': ['base'],
     'data': ['views.xml'],
-    'installable': False,
+    'installable': True,
 }
