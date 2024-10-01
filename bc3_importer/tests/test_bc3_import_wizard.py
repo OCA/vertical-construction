b1aa26ad7e610948b7a1f984a8a664a21e1dfe6d
szalatyzuzanna <szalaty.susana@gmail.com>
2024-10-01 11:12:55 +0200

    [MIG] bc3_importer: Migration to 16.0

--- bc3_importer/tests/test_bc3_import_wizard.py
+++ bc3_importer/tests/test_bc3_import_wizard.py
@@ -7,19 +7,20 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 
 class TestBC3ImportWizard(TransactionCase):
-    def setUp(self):
-        super(TestBC3ImportWizard, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         with open(os.path.join(__location__, "bc3_file_test.bc3"), "rb") as file:
             bc3_content = file.read()
-        self.bc3_import_wizard = self.env["bc3.import.wizard"].create(
+        cls.bc3_import_wizard = cls.env["bc3.import.wizard"].create(
             {
                 "bc3_file": base64.b64encode(bc3_content),
                 "bc3_file_name": "test.bc3",
-                "project_id": self.env.ref("project.project_project_1").id,
-                "version_id": self.env.ref("bc3_importer.bc3_version_2020_v2").id,
-                "partner_id": self.env.ref("base.res_partner_1").id,
+                "project_id": cls.env.ref("project.project_project_1").id,
+                "version_id": cls.env.ref("bc3_importer.bc3_version_2020_v2").id,
+                "partner_id": cls.env.ref("base.res_partner_1").id,
                 "create_products": False,
-                "sale_id": self.env.ref("sale.sale_order_1").id,
+                "sale_id": cls.env.ref("sale.sale_order_1").id,
             }
         )
 
